fb6d5d4304947e07464cdad05da8d737268fe0eb
capeya <capeya92@gmail.com>
2024-02-12 17:54:34 +0100

    add exercise

--- cpp/hello.cpp
+++ cpp/hello.cpp
@@ -1,13 +1,74 @@
 #include <iostream>
 
-double square(double x) {
+double square(double x)
+{
     return x * x;
 }
 
-void print_square(double x) {
+void print_square(double x)
+{
     std::cout << "The square of " << x << " is " << square(x) << "\n";
 }
 
-int main() {
+bool accept()
+{
+    int tries = 1;
+    while (tries < 4)
+    {
+        std::cout << "Do you want to proceed (y or n)?\n";
+        char answer = 0;
+        std::cin >> answer;
+
+        switch (answer)
+        {
+        case 'y':
+            return true;
+            break;
+        case 'n':
+            return false;
+        default:
+            std::cout << "Sorry, I don't understand that\n";
+            ++tries;
+            break;
+        }
+    }
+
+    std::cout << "I'll take that for a no.\n";
+    return false;
+}
+
+void print()
+{
+    int v1[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int v2[10];
+
+    for (auto i = 0; i != 10; ++i)
+    {
+        v2[i] = v1[i];
+    }
+
+    for (auto x : v1)
+    {
+        std::cout << x << "\n";
+    }
+}
+
+int count_x(char *p, char x)
+{
+    if (p == nullptr)
+        return 0;
+    int count = 0;
+    for (; *p != 0; ++p)
+        if (*p == x)
+            ++count;
+    return count; 
+}
+
+int main()
+{
     print_square(1.234);
+
+    accept();
+
+    print();
 }
